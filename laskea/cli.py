d80ecf9acdd3745f238fa5169ab2548f201e6f77
Stefan Hagen <stefan@dilettant.eu>
2022-02-26 07:36:43 +0100

    Create cli.py

---
+++ laskea/cli.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long
+"""Commandline API gateway for laskea."""
+import sys
+from typing import List, Union
+
+import typer
+
+import laskea
+import laskea.laskea as fill
+
+APP_NAME = 'Calculate (Finnish: laskea) some parts..'
+APP_ALIAS = 'laskea'
+app = typer.Typer(
+    add_completion=False,
+    context_settings={'help_option_names': ['-h', '--help']},
+    no_args_is_help=True,
+)
+
+
+@app.callback(invoke_without_command=True)
+def callback(
+    version: bool = typer.Option(
+        False,
+        '-V',
+        '--version',
+        help='Display the laskea version and exit',
+        is_eager=True,
+    )
+) -> None:
+    """
+    Calculate (Finnish: laskea) some parts..
+    """
+    if version:
+        typer.echo(f'{APP_NAME} version {laskea.__version__}')
+        raise typer.Exit()
