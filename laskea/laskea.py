fb67cd579a444084947226522ef2f76e24ee49b5
Stefan Hagen <stefan@dilettant.eu>
2022-02-26 07:40:56 +0100

    Create laskea.py

---
+++ laskea/laskea.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=expression-not-assigned,line-too-long
+"""Calculate (Finnish: laskea) some parts."""
+
+
+def init():
+    """Minimize boilerplate in the target documents."""
+    pass
