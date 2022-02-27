ac66c064d415f94b1673b0ce4439e1f07ed2061a
Stefan Hagen <stefan@hagen.link>
2022-02-27 15:25:13 +0100

    Cleansed self reflection and now add the next revision - mind bendingly yours ...

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -1,7 +1,7 @@
 """Calculate (Finnish: laskea) some parts.."""
 # [[[fill git_describe()]]]
-__version__ = '2022.2.27+sha1.6d5d53ba-dirty'
-# [[[end]]] (checksum: 09441222a47fbf41a0b3e49fe3cc8c61)
+__version__ = '2022.2.27+sha1.b9032cc2'
+# [[[end]]] (checksum: 9b326e7e541db02c1424e9a1b36a7c2f)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'sha1'
 )
