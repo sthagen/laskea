23e77923af0da383a20286bb4e0fd1797a36405f
Stefan Hagen <stefan@hagen.link>
2022-02-27 15:41:16 +0100

    Spiraling up or down, I really don't know ...

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -1,7 +1,7 @@
 """Calculate (Finnish: laskea) some parts.."""
 # [[[fill git_describe()]]]
-__version__ = '2022.2.27+parent.d0500609-dirty'
-# [[[end]]] (checksum: c3a40ec31b4476da2163e148ea826146)
+__version__ = '2022.2.27+parent.f5e60832'
+# [[[end]]] (checksum: 9cdcf169afd9989f50a4054e03853712)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
