6d5d53bae28b99f43f8b67f0c5efbd11ac197c22
Stefan Hagen <stefan@hagen.link>
2022-02-27 14:40:08 +0100

    Cleansing tests

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -1,3 +1,5 @@
 """Calculate (Finnish: laskea) some parts.."""
-__version__ = '2022.2.26'
+# [[[fill git_describe(always=True)]]]
+__version__ = '2022.2.26+sha1.cafebad'
+# [[[end]]]
 __version_info__ = tuple(__version__.split('.'))
