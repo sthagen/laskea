c7c2a8dfa60691cf04ee1ea08146fd1627ce341f
Stefan Hagen <stefan@hagen.link>
2022-02-27 17:37:37 +0100

    The child revision ...

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -1,7 +1,7 @@
 """Calculate (Finnish: laskea) some parts.."""
 # [[[fill git_describe()]]]
-__version__ = '2022.2.27+parent.23e77923-dirty'
-# [[[end]]] (checksum: cee958228a2f2660e9a16aa328d43132)
+__version__ = '2022.2.27+parent.5fdd0a49'
+# [[[end]]] (checksum: 5b8557338958e372a49c6321f2752069)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
