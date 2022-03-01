e3967888a9d009567e1abb262a5e83bf8bc99f04
Stefan Hagen <stefan@hagen.link>
2022-03-01 21:18:11 +0100

    Defined parent version

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -3,8 +3,8 @@ from laskea.api.jira import login, markdown_table, query
 from laskea.laskea import table
 
 # [[[fill git_describe()]]]
-__version__ = '2022.3.1+parent.7efa1c89-dirty'
-# [[[end]]] (checksum: 58530d5151e77b9024245cb19f2801a7)
+__version__ = '2022.3.1+parent.989851d8'
+# [[[end]]] (checksum: 2ec216157bdb20fe4e2f061d2ea05498)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
