5fc75d4cd5e62ca0048ba6ce60c8366576115dd3
Stefan Hagen <stefan@hagen.link>
2022-03-01 20:42:32 +0100

    Removed need for python glue code per default imports (and env vars)

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -5,3 +5,6 @@ __version__ = '2022.2.28+parent.39977ab5'
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
+from laskea.laskea import table
+from laskea.api.jira import login, markdown_table, query
+__all__ = ['login', 'markdown_table', 'query', 'table']
