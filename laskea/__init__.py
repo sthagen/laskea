1e3cccca30e21fe797db2ba2244f408381b7a83b
Stefan Hagen <stefan@hagen.link>
2022-03-01 21:16:22 +0100

    Trial and error ...

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -3,8 +3,8 @@ from laskea.api.jira import login, markdown_table, query
 from laskea.laskea import table
 
 # [[[fill git_describe()]]]
-__version__ = '2022.2.28+parent.39977ab5'
-# [[[end]]] (checksum: 0fa8d9244fa668303dc9fc7395bfb5a9)
+__version__ = '2022.3.1+parent.7efa1c89-dirty'
+# [[[end]]] (checksum: 58530d5151e77b9024245cb19f2801a7)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
