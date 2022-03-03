adbd687dde204af5a1ce8be1e79387dfad2472a3
Stefan Hagen <stefan@hagen.link>
2022-03-04 00:20:57 +0100

    Version sync

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -3,8 +3,8 @@ from laskea.api.jira import login, markdown_table, query
 from laskea.laskea import table
 
 # [[[fill git_describe()]]]
-__version__ = '2022.3.3+parent.cc2efded'
-# [[[end]]] (checksum: bcdcd2a933ea0f0eca66c65242898509)
+__version__ = '2022.3.3+parent.22b5b468'
+# [[[end]]] (checksum: 45ca77c568dddeda3a8004c2106a7422)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
