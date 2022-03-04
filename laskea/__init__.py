c69b9593e3c0999db24c123e348683473a6dd427
Stefan Hagen <stefan@dilettant.eu>
2022-03-04 15:16:05 +0100

    Update __init__.py

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -3,8 +3,8 @@ from laskea.api.jira import login, markdown_table, markdown_unordered_list, quer
 from laskea.laskea import table, ul
 
 # [[[fill git_describe()]]]
-__version__ = '2022.3.3+parent.22b5b468'
-# [[[end]]] (checksum: 45ca77c568dddeda3a8004c2106a7422)
+__version__ = '2022.3.4+parent.ec1ce47e'
+# [[[end]]] (checksum: 29cf44ab70f2ac50ceb0372326df1ab8)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
