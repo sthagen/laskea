ae2338a0f7dab498732b3103ab83a03389d65392
Stefan Hagen <stefan@dilettant.eu>
2022-03-04 15:12:19 +0100

    Update __init__.py

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -1,6 +1,6 @@
 """Calculate (Finnish: laskea) some parts.."""
-from laskea.api.jira import login, markdown_table, query
-from laskea.laskea import table
+from laskea.api.jira import login, markdown_table, markdown_unordered_list, query
+from laskea.laskea import table, ul
 
 # [[[fill git_describe()]]]
 __version__ = '2022.3.3+parent.22b5b468'
@@ -8,4 +8,4 @@ __version__ = '2022.3.3+parent.22b5b468'
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
-__all__ = ['login', 'markdown_table', 'query', 'table']
+__all__ = ['login', 'markdown_table', 'markdown_unordered_list', 'query', 'table', 'ul']
