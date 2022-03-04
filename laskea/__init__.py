7806167e7724b1d073217219756d00611d12e95d
Stefan Hagen <stefan@hagen.link>
2022-03-04 23:59:17 +0100

    Prepare next version in alternate build chain

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -3,8 +3,8 @@ from laskea.api.jira import login, markdown_list, markdown_table, query
 from laskea.laskea import ol, table, ul
 
 # [[[fill git_describe()]]]
-__version__ = '2022.3.4+parent.d7131397'
-# [[[end]]] (checksum: 0b17d075b5b472ebae045546aef318da)
+__version__ = '2022.3.5+parent.3bea2cd0-dirty'
+# [[[end]]] (checksum: 993127116cfb50d8a34588146222ec98)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
