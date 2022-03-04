314a30bd39c601d727fb1a4c9d79a0267fe26ba2
Stefan Hagen <stefan@hagen.link>
2022-03-04 23:30:17 +0100

    Added version of parent for next release 2022.3.4

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -3,8 +3,8 @@ from laskea.api.jira import login, markdown_list, markdown_table, query
 from laskea.laskea import ol, table, ul
 
 # [[[fill git_describe()]]]
-__version__ = '2022.3.4+parent.971481a1-dirty'
-# [[[end]]] (checksum: 87de98208f43c9226ac366d906da1234)
+__version__ = '2022.3.4+parent.d7131397'
+# [[[end]]] (checksum: 0b17d075b5b472ebae045546aef318da)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
