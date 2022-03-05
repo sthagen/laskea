0d92aa312d5a48d680bf44761681274691e69637
Stefan Hagen <stefan@hagen.link>
2022-03-05 01:14:18 +0100

    And version chores will never end ...

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -3,8 +3,8 @@ from laskea.api.jira import login, markdown_list, markdown_table, query
 from laskea.laskea import ol, table, ul
 
 # [[[fill git_describe()]]]
-__version__ = '2022.3.5+parent.d9d20239'
-# [[[end]]] (checksum: 5d21a5fa2fa0abf7dc5beeb65301be1c)
+__version__ = '2022.3.5+parent.7d561a50'
+# [[[end]]] (checksum: ab94f68d3d141381d8c17d52c1831302)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
