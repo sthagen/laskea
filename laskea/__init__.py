88a386257d4317abdf2fdd9723929ce8abf25c9e
Stefan Hagen <stefan@hagen.link>
2022-03-06 17:37:52 +0100

    Parent revision tag for caching version of laskea 2022.3.7

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -5,8 +5,8 @@ from laskea.api.jira import login, markdown_heading, markdown_list, markdown_tab
 from laskea.embed import dl, h1, h2, h3, h4, h5, h6, ol, table, ul
 
 # [[[fill git_describe()]]]
-__version__ = '2022.3.7+parent.fe9649cb-dirty'
-# [[[end]]] (checksum: ddaa75604fadeaa072942fdda86a56b9)
+__version__ = '2022.3.7+parent.ebcee883'
+# [[[end]]] (checksum: b33e332a8d2af21ba7e4a7739e724ab8)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
