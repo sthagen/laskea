c9624affb64a790142468d2329cd0130892e1ab5
Stefan Hagen <stefan@hagen.link>
2022-03-06 06:39:40 +0100

    Wun off ... again

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -5,8 +5,8 @@ from laskea.api.jira import login, markdown_heading, markdown_list, markdown_tab
 from laskea.embed import dl, h1, h2, h3, h4, h5, h6, ol, table, ul
 
 # [[[fill git_describe()]]]
-__version__ = '2022.3.6+parent.a317bdb1-dirty'
-# [[[end]]] (checksum: 794bbdfab3cd02e91d8af87bfa1bf65d)
+__version__ = '2022.3.6+parent.2422594d-dirty'
+# [[[end]]] (checksum: f0e79fde037fbaaa9187d6fd7a16c6c3)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
