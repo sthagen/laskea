ab09513aaf24301df8b59f1e92c6e219cafcac0c
Stefan Hagen <stefan@hagen.link>
2022-03-06 18:11:16 +0100

    Commits are cheap

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -5,8 +5,8 @@ from laskea.api.jira import login, markdown_heading, markdown_list, markdown_tab
 from laskea.embed import dl, h1, h2, h3, h4, h5, h6, ol, table, ul
 
 # [[[fill git_describe()]]]
-__version__ = '2022.3.7+parent.4a9a4a3d'
-# [[[end]]] (checksum: 08502b716bda02c0897e3a9b43583074)
+__version__ = '2022.3.8+parent.8b65372f-dirty'
+# [[[end]]] (checksum: fde9eef7571001a80f0fcfd5dfe8770b)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
