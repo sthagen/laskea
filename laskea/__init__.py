a50aac3f44078a9ce50684949a10269c5accc92a
Stefan Hagen <stefan@hagen.link>
2022-03-06 14:33:14 +0100

    Bumped parent commit indicator for implementation version (again)

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -5,8 +5,8 @@ from laskea.api.jira import login, markdown_heading, markdown_list, markdown_tab
 from laskea.embed import dl, h1, h2, h3, h4, h5, h6, ol, table, ul
 
 # [[[fill git_describe()]]]
-__version__ = '2022.3.6+parent.e8758ad7'
-# [[[end]]] (checksum: 93533e207e71b4a68dca447946690e3d)
+__version__ = '2022.3.6+parent.0d08af64'
+# [[[end]]] (checksum: 3e7e748efd160fd5bd7ca3fee55e660a)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
