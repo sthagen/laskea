e574fc04158923e5a398cd6f39f9a9593edf01b1
Stefan Hagen <stefan@hagen.link>
2022-06-06 16:48:55 +0200

    Bumped version of implementation

--- laskea/__init__.py
+++ laskea/__init__.py
@@ -66,8 +66,8 @@ from laskea.api.jira import login, markdown_heading, markdown_list, markdown_tab
 from laskea.embed import dl, h1, h2, h3, h4, h5, h6, kpi_table, metrics_table, ol, table, test_plans, ul  # noqa
 
 # [[[fill git_describe()]]]
-__version__ = '2022.5.31+parent.3c34b056'
-# [[[end]]] (checksum: fbafc96a6fb3a3456576b44feb785dad)
+__version__ = '2022.6.6+parent.04c306e3'
+# [[[end]]] (checksum: bebf1da1d34caaeaa502ea6f735bc442)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
