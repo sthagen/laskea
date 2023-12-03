a33564d79fb6a1e4d1cb67b3141956d9af7cdfd0
Stefan Hagen <stefan@hagen.link>
2023-12-03 16:27:35 +0100

    Added seed module for the transformer class

---
+++ laskea/transform.py
@@ -0,0 +1,80 @@
+"""Transformer API for laskea.
+
+Typical filter data comes in as JSON and maps column keys to filter tasks:
+
+{
+  "order": ["keep", "drop", "replace"],
+  "keep": [
+    ["startswith", "ABC-"],
+    ["contains", "Z"],
+    ["icontains", "m"],
+    ["equals", "DEF-42"],
+    ["endswith", "-123"]
+  ],
+  "drop": [
+    ["matches", "[A-Z]+-\\d+"]
+  ],
+  "replace": [
+    ["DEF-", "definition-"]
+  ]
+]
+}
+
+"""
+from laskea import FILTER_MAP_TYPE, FILTER_ORDER_TYPE, FILTER_PAYLOAD_TYPE
+
+
+class FilterMap:
+    """The class FilterMap validates the task data against known operations and required arguments.
+
+    Known operations are:
+
+    - drop
+    - keep
+    - replace
+
+    A meta operation is:
+
+    - order
+
+    This "operation" is optional but if present must fully specify the order of application of the "real" operations.
+
+    The default order of application is
+
+    1. keep
+    2. drop
+    3. replace
+
+    Real operation JSON member values are a list of list of strings (the payloads).
+    The payloads have length two with semantics depending on the operation.
+
+    Operations keep amd drop both iterate over all payloads in the order given
+    by applying the action encoded in the first list item and using the second item as parameter
+    on the cell content (list of strings) elements.
+
+    The encoding of actions is as follows (for cell entry `entry` and payload parameter `that`):
+
+    contains - `that in entry`
+    endswith - `entry.endswith(that)`
+    equals - `that == entry`
+    icontains - `that.lower() in entry.lower()`
+    iendswith - `entry.lower().endswith(that.lower())`
+    iequals - `that.lower() == entry.lower()`
+    istartswith - `entry.lower().startswith(that.lower())`
+    matches - `re.compile(that).matches(entry)`
+    startswith - `entry.startswith(that)`
+
+    The third operation (replace) is delegated to the string replace function as action.
+    In this case a payload pair like ["this", "with that"] is applied as `entry.replace('this', 'with that')`.
+
+    """
+
+    ORDER: FILTER_ORDER_TYPE = ['keep', 'drop', 'replace']
+
+    def __init__(self, column: str, filter_data: FILTER_MAP_TYPE):
+        self.column = column
+        self.filter_data: FILTER_MAP_TYPE = filter_data
+        self.order = self.filter_data['order'] if self.filter_data.get('order', []) else FilterMap.ORDER
+        self.keeps: FILTER_PAYLOAD_TYPE = self.filter_data.get('keep', None)
+        self.drops: FILTER_PAYLOAD_TYPE = self.filter_data.get('drop', None)
+        self.replaces: FILTER_PAYLOAD_TYPE = self.filter_data.get('replace', None)
