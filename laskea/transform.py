c238749faae947db88ebf2cb0647bbd60449b396
Stefan Hagen <stefan@hagen.link>
2023-12-03 19:42:10 +0100

    Added more tests for and fixed bootstrap errors in transform

--- laskea/transform.py
+++ laskea/transform.py
@@ -21,13 +21,95 @@ Typical filter data comes in as JSON and maps column keys to filter tasks:
 }
 
 """
+import re
 from laskea import FILTER_MAP_TYPE, FILTER_ORDER_TYPE, FILTER_PAYLOAD_TYPE, log
 
-
+# operation keys
 DROP = 'drop'
 KEEP = 'keep'
 ORDER = 'order'
 REPLACE = 'replace'
+
+# action keys
+CONTAINS = 'contains'
+ENDSWITH = 'endswith'
+EQUALS = 'equals'
+ICONTAINS = 'icontains'
+IENDSWITH = 'iendswith'
+IEQUALS = 'iequals'
+ISTARTSWITH = 'istartswith'
+MATCHES = 'matches'
+STARTSWITH = 'startswith'
+
+ACTION_KEYS = (
+    CONTAINS,
+    ENDSWITH,
+    EQUALS,
+    ICONTAINS,
+    IENDSWITH,
+    IEQUALS,
+    ISTARTSWITH,
+    MATCHES,
+    STARTSWITH,
+)
+
+
+def op_contains(entry: str, that: str) -> bool:
+    """Case sensitive contains."""
+    return bool(that in entry)
+
+
+def op_endswith(entry: str, that: str) -> bool:
+    """Case sensitive ends with."""
+    return bool(entry.endswith(that))
+
+
+def op_equals(entry: str, that: str) -> bool:
+    """Case sensitive equals."""
+    return bool(that == entry)
+
+
+def op_icontains(entry: str, that: str) -> bool:
+    """Case insensitive contains."""
+    return bool(that.lower() in entry.lower())
+
+
+def op_iendswith(entry: str, that: str) -> bool:
+    """Case insensitive ends with."""
+    return bool(entry.lower().endswith(that.lower()))
+
+
+def op_iequals(entry: str, that: str) -> bool:
+    """Case insensitive equals."""
+    return bool(that.lower() == entry.lower())
+
+
+def op_istartswith(entry: str, that: str) -> bool:
+    """Case insensitive starts with."""
+    return bool(entry.lower().startswith(that.lower()))
+
+
+def op_matches(entry: str, that: str) -> bool:
+    """Matches regular expression."""
+    return bool(re.compile(that).match(entry))
+
+
+def op_startswith(entry: str, that: str) -> bool:
+    """Case sensitive starts with."""
+    return bool(entry.startswith(that))
+
+
+ACTION_MAP = {
+    CONTAINS: op_contains,
+    ENDSWITH: op_endswith,
+    EQUALS: op_equals,
+    ICONTAINS: op_icontains,
+    IENDSWITH: op_iendswith,
+    IEQUALS: op_iequals,
+    ISTARTSWITH: op_istartswith,
+    MATCHES: op_matches,
+    STARTSWITH: op_startswith,
+}
 
 
 class FilterMap:
@@ -67,7 +149,7 @@ class FilterMap:
     iendswith - `entry.lower().endswith(that.lower())`
     iequals - `that.lower() == entry.lower()`
     istartswith - `entry.lower().startswith(that.lower())`
-    matches - `re.compile(that).matches(entry)`
+    matches - `re.compile(that).match(entry)`
     startswith - `entry.startswith(that)`
 
     The third operation (replace) is delegated to the string replace function as action.
@@ -80,29 +162,51 @@ class FilterMap:
     def __init__(self, column: str, filter_data: FILTER_MAP_TYPE):
         self.column = column
         self.filter_data: FILTER_MAP_TYPE = filter_data
+
         self.order = self.filter_data[ORDER] if self.filter_data.get(ORDER, []) else FilterMap.ORDER
-        self.keeps: FILTER_PAYLOAD_TYPE = self.filter_data.get(KEEP, [[]])  # type: ignore
-        self.drops: FILTER_PAYLOAD_TYPE = self.filter_data.get(DROP, [[]])  # type: ignore
-        self.replaces: FILTER_PAYLOAD_TYPE = self.filter_data.get(REPLACE, [[]])  # type: ignore
-        self.tasks = []
-        for operation in self.order:
-            if operation == KEEP:
-                self.tasks.append((KEEP, self.keeps))
-            elif operation == DROP:
-                self.tasks.append((DROP, self.drops))
-            elif operation == REPLACE:
-                self.tasks.append((REPLACE, self.replaces))
+        log.debug(f'{self.order=}')
+
+        self.keeps: FILTER_PAYLOAD_TYPE = self.filter_data.get(KEEP, [])  # type: ignore
+        self.drops: FILTER_PAYLOAD_TYPE = self.filter_data.get(DROP, [])  # type: ignore
+        self.replaces: FILTER_PAYLOAD_TYPE = self.filter_data.get(REPLACE, [])  # type: ignore
+
+        self.operations = []
+        for kind in self.order:
+            if kind == KEEP:
+                if self.keeps:
+                    self.operations.append((kind, self.keeps))
+            elif kind == DROP:
+                if self.drops:
+                    self.operations.append((kind, self.drops))
+            elif kind == REPLACE:
+                if self.replaces:
+                    self.operations.append((kind, self.replaces))
             else:
-                log.warning(f'ignored order element ({operation}) - please verify your filter data')
+                log.warning(f'ignored order element ({kind}) - please verify your filter data')
 
     def apply(self, entry: str) -> str:
         """Initial naive application during stage 1 implementation of transformer."""
         if not entry.strip():  # TODO(sthagen) - this may exclude use cases of manipulating space ;-)
             return ''
-        if not self.tasks:
+        if not self.operations:
             return entry
         transformed = entry
-        for kind, task in self.tasks:
-            pass
+        for kind, tasks in self.operations:
+            if kind in (KEEP, DROP):
+                if tasks:
+                    for key, parameter in tasks:
+                        if key.lower() not in ACTION_KEYS:
+                            log.warning(f'skipping action with unknown key ({key}) for operation type ({kind})')
+                            continue
+                        hit = ACTION_MAP[key.lower()](transformed, parameter)
+                        if hit:
+                            if kind == DROP:
+                                return ''
+                            if kind == KEEP:
+                                break
+            else:  # REPLACE
+                if tasks:
+                    for this, with_that in tasks:
+                        transformed = transformed.replace(this, with_that)
 
         return transformed
--- test/test_transform.py
+++ test/test_transform.py
@@ -19,3 +19,16 @@ def test_apply_spaces_only():
 def test_apply_no_filters():
     c_filter = tr.FilterMap('c', {})
     assert c_filter.apply('foo') == 'foo'
+
+
+def test_apply_drop_that():
+    c_filter = tr.FilterMap('c', {'drop': [['equals', 'that']]})
+    assert c_filter.apply('that') == ''
+
+
+def test_op_equals_hits():
+    assert tr.op_equals('entry', 'entry')
+
+
+def test_op_equals_misses():
+    assert not tr.op_equals('entry', 'different')
