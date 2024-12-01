5c98a0ef32efe62176c6a1d864b01b34cbaf3a46
Stefan Hagen <stefan@hagen.link>
2024-12-01 20:15:34 +0100

    Solved a deprecation for report
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- laskea/env.py
+++ laskea/env.py
@@ -1,6 +1,6 @@
 """Report environment to support resolution of user issues."""
 
-import sys
+import pathlib
 from typing import no_type_check
 
 import scooby
@@ -9,26 +9,18 @@ import scooby
 @no_type_check
 def report(shallow: bool = False) -> str:
     """Return either text options for the user to report her env or the report of the environment for support."""
-    deep = True
-    try:
-        import pkg_resources  # noqa
-    except (ImportError, ModuleNotFoundError):
-        if shallow:
-            deep = False
-            sys.stdout.write('\nInfo: No setuptools module found and shallow reporting requested. Continuing ...')
-        else:
-            message = (
-                '\n  Report requires setuptools (to identify the version of atlassian-python-api).'
-                '\n  You may either add --shallow or install the module via `pip install setuptools`\n\n'
-            )
-            return message
+    import importlib.resources  # noqa
+    import atlassian  # type: ignore # noqa
 
-    if deep:
-        import atlassian  # type: ignore # noqa
-
-        packages = pkg_resources.working_set  # noqa
-        monkey_atl = [p.version for p in packages if p.project_name == 'atlassian-python-api'][0]  # noqa
-        atlassian.__version__ = monkey_atl
+    version_path = pathlib.Path(importlib.resources.files('atlassian')) / 'VERSION'  # noqa
+    monkey_atl = '... should register a version - workaround failed (VERSION)'
+    if version_path.is_file():
+        with open(version_path) as version_file:
+            try:
+                monkey_atl = version_file.read().strip()
+            except Exception:  # noqa
+                pass
+    atlassian.__version__ = monkey_atl
 
     class Report(scooby.Report):
         def __init__(self, additional=None, ncol=3, text_width=80, sort=False):
